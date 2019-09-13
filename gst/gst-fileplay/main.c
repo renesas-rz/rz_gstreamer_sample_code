70ce6dca4b776d64a3cc37188763ba098eb75b18
Loc Pham <loc.pham.xh@renesas.com>
2019-09-13 13:35:02 +0700

    gst-fileplay: Fix compilation warnings
    Without this patch, gst-fileplay will generate the below errors when compiling:
    
      | main.c: In function 'get_resolution_from_weston_info': main.c:50:3: warning: implicit declaration of function 'system'
      | [-Wimplicit-function-declaration]
      |    system(COMMAND_GET_WESTON_INFO); ^~~~~~ main.c:57:5: warning: implicit declaration of function 'exit'
      | [-Wimplicit-function-declaration]
      |      exit(1); ^~~~ main.c:57:5: warning: incompatible implicit declaration of built-in function 'exit' main.c:57:5: note:
      | include '<stdlib.h>' or provide a declaration of 'exit' main.c:95:49: warning: implicit declaration of function 'strtol';
      | did you mean 'strtok'? [-Wimplicit-function-declaration]
      |            availbe_width_screen[count_screens] = strtol(str_width, &ptr, LENGTH_STRING);
      |                                                  ^~~~~~
      |                                                  strtok
    
    Signed-off-by: Loc Pham <loc.pham.xh@renesas.com>

--- gst/gst-fileplay/main.c
+++ gst/gst-fileplay/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <gst/gst.h>
+#include <stdlib.h>
 
 #define INPUT_FILE                 "/home/media/videos/sintel_trailer-720p.mp4"
 #define LOCATION_FILE_WESTON_INFO  "/home/media/data_westoninfo.txt"
