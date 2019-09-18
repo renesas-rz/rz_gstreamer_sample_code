8d7b96595e7593a0a099c0effdd7d055b6163ef5
Loc Pham <loc.pham.xh@renesas.com>
2019-09-18 14:03:59 +0700

    gst-videoplay: Use wayland API directly instead of parsing generated output of "weston-info" command
    Signed-off-by: Loc Pham <loc.pham.xh@renesas.com>

--- gst/gst-videoplay/main.c
+++ gst/gst-videoplay/main.c
@@ -2,117 +2,315 @@
 #include <string.h>
 #include <gst/gst.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <wayland-client.h>
 
 #define INPUT_FILE                 "/home/media/videos/vga1.h264"
-#define LOCATION_FILE_WESTON_INFO  "/home/media/data_westoninfo.txt"
-#define COMMAND_GET_WESTON_INFO    "weston-info >& /home/media/data_westoninfo.txt"
-#define SUBSTRING_WIDTH            "width: "
-#define SUBSTRING_HEIGHT           "height: "
-#define SUBSTRING_REFRESH          "refresh"
-#define SUBSTRING_FLAGS            "flags: current"
 #define POSITION_X           0
 #define POSITION_Y           0
-#define LENGTH_STRING        10
-#define INDEX_WESTON_WIDTH   6
-#define INDEX_WESTON_HEIGHT  23
 
-/* Count number of available screens */
-int count_screens = 0;
-/* Declare variable arrays for resolution of screen */
-int availbe_width_screen[10];
-int availbe_height_screen[10];
+/* These structs contain information needed to get a list of available screens */
+struct screen_t
+{
+  uint16_t x;
+  uint16_t y;
+
+  uint16_t width;
+  uint16_t height;
+
+  struct wl_list link;
+};
+
+struct wayland_t
+{
+  struct wl_display *display;
+  struct wl_registry *registry;
+  struct wl_output *output;
+
+  struct wl_list screens;
+};
+
+/*
+ *
+ * name: init_wayland
+ * Initialize "wayland_t" structure
+ *
+ */
+void
+init_wayland(struct wayland_t *handler)
+{
+  if (handler != NULL)
+  {
+    /* Initialize "wayland_t" structure */
+    handler->display = NULL;
+    handler->registry = NULL;
+    handler->output = NULL;
+
+    /* Initialize doubly-linked list */
+    wl_list_init(&(handler->screens));
+  }
+}
+
+/*
+ *
+ * name: destroy_wayland
+ * De-initialize wayland_t structure
+ *
+ */
+void
+destroy_wayland(struct wayland_t *handler)
+{
+  struct screen_t *screen = NULL;
+  struct screen_t *tmp = NULL;
+
+  if (handler != NULL)
+  {
+    /* Clean up screens */
+    if (!wl_list_empty(&(handler->screens)))
+    {
+      wl_list_for_each_safe(screen, tmp, &(handler->screens), link)
+      {
+        wl_list_remove(&(screen->link));
+        free(screen);
+      }
+    }
+
+    /* Clean up wayland */
+    if (handler->output != NULL)
+    {
+      wl_output_destroy(handler->output);
+    }
+
+    if (handler->registry != NULL)
+    {
+      wl_registry_destroy(handler->registry);
+    }
+
+    if (handler->display != NULL)
+    {
+      wl_display_disconnect(handler->display);
+    }
+
+    /* De-allocate "wayland_t" structure itself */
+    free(handler);
+  }
+}
+
+/*
+ *
+ * name: output_handle_geometry
+ * Obtain geometry information, such as: name, model, physical width,
+ * physical height...
+ *
+ */
+static void
+output_handle_geometry(void *data, struct wl_output *wl_output,
+		       int32_t x, int32_t y,
+		       int32_t physical_width, int32_t physical_height,
+		       int32_t subpixel,
+		       const char *make, const char *model,
+		       int32_t output_transform)
+{
+  struct screen_t *screen = (struct screen_t*)data;
+
+  screen->x = x;
+  screen->y = y;
+}
+
+/*
+ *
+ * name: output_handle_mode
+ * Obtain screen's information, such as: width, height, refresh rate....
+ *
+ */
+static void
+output_handle_mode(void *data, struct wl_output *wl_output,
+		   uint32_t flags, int32_t width, int32_t height,
+		   int32_t refresh)
+{
+  struct screen_t *screen = (struct screen_t*)data;
+  if (flags & WL_OUTPUT_MODE_CURRENT)
+  {
+    screen->width = width;
+    screen->height = height;
+  }
+}
+
+/*
+ *
+ * name: output_handle_scale
+ * Obtain geometry scale
+ *
+ */
+static void
+output_handle_scale(void *data, struct wl_output *wl_output,
+		    int32_t scale)
+{
+  /* Do nothing */
+}
+
+static void
+output_handle_done(void *data, struct wl_output *wl_output)
+{
+  /* Don't bother waiting for this; there's no good reason a
+   * compositor will wait more than one roundtrip before sending
+   * these initial events. */
+}
+
+/* This variable is used to get information from global object "wl_outout" */
+static const struct wl_output_listener output_listener =
+{
+  output_handle_geometry,
+  output_handle_mode,
+  output_handle_done,
+  output_handle_scale,
+};
+
+/*
+ *
+ * name: global_handler
+ * Register global objects from Wayland compositor
+ *
+ */
+static void
+global_handler(void *data, struct wl_registry *registry, uint32_t id,
+	       const char *interface, uint32_t version)
+{
+  struct screen_t *screen = NULL;
+  struct wayland_t *handler = (struct wayland_t*)data;
+
+  if (strcmp(interface, "wl_output") == 0)
+  {
+    /* Allocate and initialize "screen_t" structure */
+    screen = (struct screen_t*)calloc(1, sizeof(struct screen_t));
+
+    if (screen != NULL)
+    {
+      handler->output = wl_registry_bind(handler->registry, id, &wl_output_interface, MIN(version, 2));
+      wl_output_add_listener(handler->output, &output_listener, screen);
+
+      /* Wait until all screen's data members are filled */
+      wl_display_roundtrip(handler->display);
+
+      if ((screen->width == 0) || (screen->height == 0))
+      {
+        /* Remove invalid screen */
+        free(screen);
+      }
+      else
+      {
+        /* Add this new screen to the head of doubly-linked list */
+        wl_list_insert(&(handler->screens), &(screen->link));
+      }
+    }
+  }
+}
+
+/*
+ *
+ * name: global_remove_handler
+ * Remove public objects from Wayland compositor
+ *
+ */
+static void
+global_remove_handler(void *data, struct wl_registry *registry, uint32_t name)
+{
+  /* Do nothing */
+}
+
+/* This variable contains functions to register public Wayland's objects */
+static const struct wl_registry_listener registry_listener =
+{
+  global_handler,
+  global_remove_handler
+};
 
 /*
  * 
- * name: get_resolution_from_weston_info
- * Get resolution from "weston-info" 
+ * name: get_available_screens
+ * Get a list of available screens
  * 
  */
-static void get_resolution_from_weston_info(int *availbe_width_screen, int *availbe_height_screen)
+struct wayland_t*
+get_available_screens()
 {
-  /* Initial variables  */
-  char *line = NULL;
-  size_t len, read;
-  FILE *fp;
-  char p_save_res_line[100];
+  struct wayland_t *handler = calloc(1, sizeof(struct wayland_t));
+  if (handler == NULL)
+  {
+    return NULL;
+  }
 
-  /* 
-   * To show the usage of system() function to list down all weston-info
-   * in the specified file.
-   */
-  system(COMMAND_GET_WESTON_INFO);
+  /* Initialize "wayland_t" structure */
+  init_wayland(handler);
 
-  /* Open data_westoninfo.txt to read width and height */
-  fp = fopen(LOCATION_FILE_WESTON_INFO,"rt");
+  /* Connect to weston compositor */
+  handler->display = wl_display_connect(NULL);
+  if (handler->display == NULL)
+  {
+    fprintf(stderr, "Failed to create display\n");
+    free(handler);
 
-  if(fp == NULL) {
-    g_printerr ("Can't open file.\n");
-    exit(1);
-  }else {
-    while((read = getline(&line, &len, fp)) != -1) {
-      /*Find the first occurrence of the substring needle in the string. */
-      char *p_width   = strstr(line, SUBSTRING_WIDTH);
-      char *p_height  = strstr(line, SUBSTRING_HEIGHT);
-      char *p_refresh = strstr(line, SUBSTRING_REFRESH);
+    return NULL;
+  }
 
-      /* 
-       * Pointer "p_width", "p_height" and "p_refresh" to the first occurrence in "line" of the 
-       * entire sequence of characters specified in "width: ", "height: " and "refresh"
-       */
-      if( p_width && p_height && p_refresh ) {
-        /* 
-         * Pointer "p_save_res_line" to "p_width" destination array where the content is to be copied.
-         * Because calling "getline" function that makes a change for pointer "p_width".
-         */
-        strncpy(p_save_res_line, p_width, strlen(p_width));
+  /* Obtain wl_registry from Wayland compositor to access public object "wl_output" */
+  handler->registry = wl_display_get_registry(handler->display);
+  wl_registry_add_listener(handler->registry, &registry_listener, handler);
 
-        /* Read more line to check resolution of the screen if it is available.*/
-        if((read = getline(&line, &len, fp)) == -1) {
-          break;
-        }
+  /* Wait until public object "wl_output" is binded */
+  wl_display_roundtrip(handler->display);
 
-        /*Find the first occurrence of the substring needle in the string. */
-        char *p_flags_current = strstr(line, SUBSTRING_FLAGS);
+  return handler;
+}
 
-        /* 
-         * Pointer "p_flags_current" to the first occurrence in "line" of the 
-         * entire sequence of characters specified in "flags: current".
-         */
-        if( p_flags_current ) {
-          char str_width[LENGTH_STRING], str_height[LENGTH_STRING];
-          char *ptr;
-  
-          /* Get available width of screen from the string. */
-          strncpy(str_width, p_save_res_line + INDEX_WESTON_WIDTH, LENGTH_STRING);
-          /* Convert sub-string to long integer. */
-          availbe_width_screen[count_screens] = strtol(str_width, &ptr, LENGTH_STRING);
+/*
+ *
+ * name: get_main_screen
+ * Get main screen which has axis (0, 0)
+ *
+ */
+struct screen_t*
+get_main_screen(struct wayland_t *handler)
+{
+  struct screen_t *result = NULL;
 
-          /* Get available height of screen from the string. */
-          strncpy(str_height, p_save_res_line + INDEX_WESTON_HEIGHT, LENGTH_STRING);
-          /* Convert sub-string to long integer. */
-          availbe_height_screen[count_screens] = strtol(str_height, &ptr, LENGTH_STRING);
-
-          count_screens++ ;
-        }
+  if ((handler != NULL) && !wl_list_empty(&(handler->screens)))
+  {
+    wl_list_for_each(result, &(handler->screens), link)
+    {
+      if ((result->x == 0) && (result->y == 0))
+      {
+        return result;
       }
     }
   }
 
-  /* Close the file */
-  fclose(fp);
+  return NULL;
 }
 
 int
 main (int argc, char *argv[])
 {
+  struct wayland_t *wayland_handler = NULL;
+  struct screen_t *main_screen = NULL;
+
   GstElement *pipeline, *source, *parser, *decoder, *sink;
   GstBus *bus;
   GstMessage *msg;
 
   const gchar *input_file = INPUT_FILE;
 
-  /* Get out-width and out-height for the out video */
-  get_resolution_from_weston_info(availbe_width_screen, availbe_height_screen);
+  /* Get a list of available screen */
+  wayland_handler = get_available_screens();
+
+  /* Get main screen */
+  main_screen = get_main_screen(wayland_handler);
+  if (main_screen == NULL)
+  {
+    g_printerr("Cannot find any available screens. Exiting.\n");
+    destroy_wayland(wayland_handler);
+    return -1;
+  }
 
   /* Initialization */
   gst_init (&argc, &argv);
@@ -126,6 +324,7 @@ main (int argc, char *argv[])
 
   if (!pipeline || !source || !parser || !decoder || !sink) {
     g_printerr ("One element could not be created. Exiting.\n");
+    destroy_wayland(wayland_handler);
     return -1;
   }
 
@@ -133,10 +332,12 @@ main (int argc, char *argv[])
   g_object_set (G_OBJECT (source), "location", input_file, NULL);
 
   /* Set position for displaying (0, 0) */
-  g_object_set (G_OBJECT (sink), "position-x", POSITION_X, "position-y", POSITION_Y, NULL);
+  g_object_set (G_OBJECT (sink), "position-x", POSITION_X, "position-y",
+      POSITION_Y, NULL);
 
   /* Set out-width and out-height for the out video */
-  g_object_set (G_OBJECT (sink), "out-width", availbe_width_screen[0], "out-height", availbe_height_screen[0], NULL);
+  g_object_set (G_OBJECT (sink), "out-width", main_screen->width,
+      "out-height", main_screen->height, NULL);
 
   /* Add all elements into the pipeline */
   /* pipeline---[ file-source + h264-parser + h264-decoder + video-output ] */
@@ -147,6 +348,7 @@ main (int argc, char *argv[])
   if (gst_element_link_many (source, parser, decoder, sink, NULL) != TRUE) {
     g_printerr ("Elements could not be linked.\n");
     gst_object_unref (pipeline);
+    destroy_wayland(wayland_handler);
     return -1;
   }
 
@@ -156,6 +358,7 @@ main (int argc, char *argv[])
           GST_STATE_PLAYING) == GST_STATE_CHANGE_FAILURE) {
     g_printerr ("Unable to set the pipeline to the playing state.\n");
     gst_object_unref (pipeline);
+    destroy_wayland(wayland_handler);
     return -1;
   }
 
@@ -197,6 +400,9 @@ main (int argc, char *argv[])
     gst_message_unref (msg);
   }
 
+  /* Clean up "wayland_t" structure */
+  destroy_wayland(wayland_handler);
+
   /* Free resources and change state to NULL */
   gst_object_unref (bus);
   g_print ("Returned, stopping playback...\n");
@@ -204,5 +410,6 @@ main (int argc, char *argv[])
   g_print ("Freeing pipeline...\n");
   gst_object_unref (GST_OBJECT (pipeline));
   g_print ("Completed. Goodbye!\n");
+
   return 0;
 }
